e6c45cc3314ba29c59dbed7f015ec8232ceca909
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-06-06 12:00:48 +0200

    Ajout du séparateur

---
+++ resources/views/components/separateur.blade.php
@@ -0,0 +1,7 @@
+
+  
+        <div class="flex items-center">
+            <div style="width: 10px; height: 10px; transform: rotate(45deg);background-color:black;"></div>
+            <hr class="w-1/2" style="border:1px solid black;">
+            <div style="width: 10px; height: 10px; transform: rotate(45deg);background-color:black;"></div>
+        </div>
--- resources/views/departementGMP.blade.php
+++ resources/views/departementGMP.blade.php
@@ -11,36 +11,60 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body style="font-family: Poppins; color:#606C38">
+<body class="font-medium" style="font-family: Poppins; color:#606C38; background-color: #F5F5F5">
 
 
     <!-- Présentation -->
-    <section class="container mx-auto">
+    <section class="container w-1/2 mx-auto mt-8">
         <h3>Présentation</h3>
         <hr>
-        <video height="759" controls>
-            <source src="movie.mp4" type="video/mp4">
-            Votre navigateur ne supporte pas la lecture de cette vidéo
-        </video>
-        <p>Le diplômé des départements Génie Mécanique et Productique des I.U.T. est un généraliste de la mécanique. Sa formation technique, scientifique, économique et humaine lui permet :
 
-            D’exercer ses activités dans tous les secteurs économiques,
-            De collaborer avec les différents acteurs du système de production,
-            De contribuer à la compétitivité des entreprises dans toutes les étapes de la vie
-            d’un produit en optimisant les choix techniques, scientifiques, économiques et humains,
-            en intégrant les impératifs de qualité, de maintenance et de sécurité.</p>
-    </section>
-    <!-- Présentation -->
+        <div class="mt-4">
+            <video height="759" controls>
+                <source src="movie.mp4" type="video/mp4">
+                Votre navigateur ne supporte pas la lecture de cette vidéo
+            </video>
+        </div>
+        <div class="mt-4">
+            <p>Le diplômé des départements Génie Mécanique et Productique des I.U.T. est un généraliste de la mécanique.
+                Sa formation technique, scientifique, économique et humaine lui permet :
 
-    <!-- Les formations du département GMP -->
-    <section class="container mx-auto">
-        <h3>Les formations du département GMP</h3>
-        <div class="rounded-lg flex" style="background-color: #DDA15E;">
-            <div><img src="" alt="Image de la licence professionnelle MRI" srcset=""></div>
-            <div><span>LP Métiers de l’industrie : mécatronique, robotique – Mécatronique – Robotique Intelligence (M.R.I)</span></div>
+            <ul class="list-disc mt-3 w-11/12 mx-auto">
+                <li>D’exercer ses activités dans tous les secteurs économiques,</li>
+                <li>De collaborer avec les différents acteurs du système de production,</li>
+                <li>De contribuer à la compétitivité des entreprises dans toutes les étapes de la vie
+                    d’un produit en optimisant les choix techniques, scientifiques, économiques et humains,
+                    en intégrant les impératifs de qualité, de maintenance et de sécurité.</li>
+            </ul>
+            </p>
         </div>
     </section>
+    <!-- Présentation -->
     <!-- Les formations du département GMP -->
+    <section class="container w-1/2 mx-auto mt-12">
+        <h3>Les formations du département GMP</h3>
+        <hr>
+        <div>
+        <div class="rounded-lg flex mt-8 h-40" style="background-color: #DDA15E;">
+            <div class="m-auto"><img src="" alt="Image de la licence professionnelle MRI" srcset=""></div>
+            <div class="text-center m-auto"><span>LP Métiers de l’industrie : mécatronique, robotique – Mécatronique – Robotique Intelligence (M.R.I)</span></div>
+        </div>
+        <x-separateur> </x-separateur>
+        </div>        <div class="rounded-lg flex mt-8 h-40" style="background-color: #DDA15E;">
+            <div class="m-auto"><img src="" alt="Image de la licence professionnelle MRI" srcset=""></div>
+            <div class="text-center m-auto"><span>LP Métiers de l’industrie : mécatronique, robotique – Mécatronique – Robotique Intelligence (M.R.I)</span></div>
+        </div>
+        <x-separateur> </x-separateur>
+        </div>        <div class="rounded-lg flex mt-8 h-40" style="background-color: #DDA15E;">
+            <div class="m-auto"><img src="" alt="Image de la licence professionnelle MRI" srcset=""></div>
+            <div class="text-center m-auto"><span>LP Métiers de l’industrie : mécatronique, robotique – Mécatronique – Robotique Intelligence (M.R.I)</span></div>
+        </div>
+        <x-separateur> </x-separateur>
+        </div>
+
+    </section>
+    <!-- Les formations du département GMP -->
+
 </body>
 
 </html>
