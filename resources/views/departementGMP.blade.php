559872322368252d7ffa0f8f467bc9636d1d7991
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-06-06 09:44:04 +0200

    Création de la page de présentation du département

---
+++ resources/views/departementGMP.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Présentation département GMP</title>
+    <meta charset="UTF-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body style="font-family: Poppins; color:#606C38">
+
+
+    <!-- Présentation -->
+    <section class="container mx-auto">
+        <h3>Présentation</h3>
+        <hr>
+        <video height="759" controls>
+            <source src="movie.mp4" type="video/mp4">
+            Votre navigateur ne supporte pas la lecture de cette vidéo
+        </video>
+        <p>Le diplômé des départements Génie Mécanique et Productique des I.U.T. est un généraliste de la mécanique. Sa formation technique, scientifique, économique et humaine lui permet :
+
+            D’exercer ses activités dans tous les secteurs économiques,
+            De collaborer avec les différents acteurs du système de production,
+            De contribuer à la compétitivité des entreprises dans toutes les étapes de la vie
+            d’un produit en optimisant les choix techniques, scientifiques, économiques et humains,
+            en intégrant les impératifs de qualité, de maintenance et de sécurité.</p>
+    </section>
+    <!-- Présentation -->
+
+    <!-- Les formations du département GMP -->
+    <section class="container mx-auto">
+        <h3>Les formations du département GMP</h3>
+        <div class="rounded-lg flex" style="background-color: #DDA15E;">
+            <div><img src="" alt="Image de la licence professionnelle MRI" srcset=""></div>
+            <div><span>LP Métiers de l’industrie : mécatronique, robotique – Mécatronique – Robotique Intelligence (M.R.I)</span></div>
+        </div>
+    </section>
+    <!-- Les formations du département GMP -->
+</body>
+
+</html>
