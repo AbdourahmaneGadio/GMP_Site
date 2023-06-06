93d51f630cdafbd55410ae112625e31df60db5cb
Abdourahmane Gadio <91066652+AbdourahmaneGadio@users.noreply.github.com>
2023-06-06 15:13:08 +0200

    LicencesPro -> Ajout header et footer

--- resources/views/licencesPro.blade.php
+++ resources/views/licencesPro.blade.php
@@ -13,7 +13,7 @@
 
 <body class="font-medium" style="font-family: Poppins; color:#606C38; background-color: #F5F5F5">
 
-
+@include('layouts.header')
     <!-- Licences professionnelles -->
     <section class="container w-1/2 mx-auto mt-8">
         <h3>Licences professionnelles (L3)</h3>
@@ -52,13 +52,14 @@
 
         </div>
         <x-separateur> </x-separateur>
-        <div class="flex mt-8 h-40" style="background-color: #DDA15E; border-radius:46px;">
+        <div class="flex mt-8 h-40 mb-8" style="background-color: #DDA15E; border-radius:46px;">
             <div class="m-auto"><img src="" alt="Image de la licence professionnelle MRI" srcset=""></div>
             <div class="text-center m-auto"><span>LP Métiers de l’industrie : mécatronique, robotique – Mécatronique – Robotique Intelligence (M.R.I)</span></div>
         </div>
 
     </section>
     <!-- Science, technologie, santé -->
+    @include('layouts.footer')
 
 </body>
 
